a19eb26649b313b00ca4aa584fe2335ad3f42c9a
George Laurentiu <bogdan.lorenzo11@gmail.com>
2021-04-17 00:46:55 +0200

    Added Task model

---
+++ src/main/java/acme/framework/repositories/TaskRepository.java
@@ -0,0 +1,20 @@
+package acme.framework.repositories;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.tasks.Task;
+
+@Repository
+@Transactional(TxType.SUPPORTS)
+public interface TaskRepository extends AbstractRepository  {
+
+	@Query("SELECT task FROM Task task where task.id = ?1")
+	Optional<Task> findOne(int id);
+	
+}
