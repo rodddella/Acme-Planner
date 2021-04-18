b85dcf7c248645283b49d6916a2001cb35748eee
CarmenMariaMP <62175263+CarmenMariaMP@users.noreply.github.com>
2021-04-18 11:57:56 +0200

    Introduced sample data of tasks

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -1,27 +1,32 @@
 package acme.features.administrator.dashboard;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import acme.framework.repositories.AbstractRepository;
 
+@Repository
 public interface AdministratorDashboardRepository extends AbstractRepository{
 	
 	@Query("select count(t) from Task t where t.visibility = 'Public'")
 	Double totalNumberOfPublicTasks ();
 	
+	
 	@Query("select count(t) from Task t where t.visibility = 'Private'")
 	Double totalNumberOfPrivateTasks ();
+	
 	
 	@Query("select count(t) from Task t where t.endPeriod <= current_timestamp()")
 	Double totalNumberOfFinishedTasks ();
 	
+	
 	@Query("select count(t) from Task t where t.endPeriod > current_timestamp()")
 	Double totalNumberOfNonFinishedTasks ();
-	
-	
+	/*
+	//@Query(value="select avg(TIMESTAMPDIFF(HOUR,t.startPeriod, t.endPeriod)) from Task t",nativeQuery = true)
 	Double averageNumberOfTaskExecutionPeriods ();
 	
-	
+	/*
 	Double deviationNumberOfTaskExecutionPeriods ();
 	
 	
@@ -29,6 +34,7 @@ public interface AdministratorDashboardRepository extends AbstractRepository{
 	
 	
 	Double maximumNumberOfTaskExecutionPeriods ();
+	*/
 	
 	@Query("select avg(t.workload) from Task t")
 	Double averageNumberOfTaskWorkloads ();
