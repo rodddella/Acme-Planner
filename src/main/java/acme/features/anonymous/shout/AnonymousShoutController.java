01cb6c496d84a80884d14c12703df7cae3b63a9f
Fco-bit <pacozf14@gmail.com>
2021-04-19 09:44:47 +0200

    Create shout by anonymous principal
    A person must register his or her name, a piece of text, and an optional link.

---
+++ src/main/java/acme/features/anonymous/shout/AnonymousShoutController.java
@@ -0,0 +1,27 @@
+package acme.features.anonymous.shout;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import acme.entities.shouts.Shout;
+import acme.framework.components.BasicCommand;
+import acme.framework.controllers.AbstractController;
+import acme.framework.entities.Anonymous;
+
+@Controller
+@RequestMapping("/anonymous/shout/")
+public class AnonymousShoutController extends AbstractController<Anonymous, Shout> {
+	
+	@Autowired
+	protected AnonymousShoutCreateService	createService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand(BasicCommand.CREATE, this.createService);
+	}
+
+}      
