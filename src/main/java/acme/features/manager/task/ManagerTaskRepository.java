f769be6196b8c4e4e5f0f75bbeb5976097e48993
Laurentiu Bogdan <bogdan.lorenzo11@gmail.com>
2021-05-17 00:58:46 +0200

    added create task test

--- src/main/java/acme/features/manager/task/ManagerTaskRepository.java
+++ src/main/java/acme/features/manager/task/ManagerTaskRepository.java
@@ -1,5 +1,7 @@
 package acme.features.manager.task;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +17,7 @@ public interface ManagerTaskRepository extends AbstractRepository {
 
 	@Query("SELECT task FROM Task task WHERE task.id = :taskId")
 	Task findTaskById(@Param("taskId") Integer taskId);
+	
+	@Query("SELECT task FROM Manager manager JOIN manager.tasks task WHERE manager.id = :managerId")
+	List<Task> findManagerTasks(@Param("managerId") Integer managerId);
 }
